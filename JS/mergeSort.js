6fe95bc7d66e07b3a46d1c8e83c0adde8da0aa83
Vishnu Vardhan Reddy Sajjala <sajjalavishnuvardhanreddy2001@gmail.com>
2023-08-10 16:39:10 +0530

    Update mergeSort.js

--- JS/mergeSort.js
+++ JS/mergeSort.js
@@ -1,45 +1,41 @@
-async function merge(arr , l , r , m){
-    let n1 = m-l+1;
-    let n2 = r-m;
+const merge = async (arr,start,end,mid)=>{
+    let n1 = mid-start+1;
+    let n2 = end-mid;
 
-    let left = new Array(n1);
-    let right = new Array(n2);
+    let leftArr = new Array(n1);
+    let rightArr = new Array(n2);
 
-    for(let i = 0; i < n1; i++){
-        arr[l+i].style.background = "lightsalmon";
-        left[i] = arr[l+i].style.height;
+    for(let i=0; i<n1; i++){
+        arr[start+i].style.background = "lightsalmon";
+        leftArr[i] = arr[start+i].style.height;
+        await wait(delay);
+    }
+    for(let i=0; i<n2; i++){
+        arr[mid+1+i].style.background = "lightseagreen";
+        rightArr[i] = arr[mid+1+i].style.height;
         await wait(delay);
     }
 
-    for(let i = 0; i < n2; i++){
-        arr[m+1+i].style.background = "lightseagreen";
-        right[i] = arr[m+1+i].style.height;
-        await wait(delay);
-    }
-
-    let i = 0 , j = 0 , k = l;
-
+    let i = 0 , j = 0 , k = start;
     while(i < n1 && j < n2){
-        if(parseInt(left[i]) <= parseInt(right[j])){
-
+        if(parseInt(leftArr[i]) <= parseInt(rightArr[j])){
             if(n1+n2 === arr.length){
                 arr[k].style.background = "-webkit-linear-gradient(rgba(238,174,202,1) 34%, rgba(148,187,233,1) 84%)";
             }
             else{
                 arr[k].style.background = "rgba(238,174,202,1)";
             }
-            arr[k].style.height = left[i];
+            arr[k].style.height = leftArr[i];
             i++;
         }
         else{
-
             if(n1+n2 === arr.length){
                 arr[k].style.background = "-webkit-linear-gradient(rgba(238,174,202,1) 34%, rgba(148,187,233,1) 84%)";
             }
             else{
                 arr[k].style.background = "rgba(238,174,202,1)";
             }
-            arr[k].style.height = right[j];
+            arr[k].style.height = rightArr[j];
             j++;
         }
         await wait(delay);
@@ -53,7 +49,7 @@ async function merge(arr , l , r , m){
         else{
             arr[k].style.background = "rgba(238,174,202,1)";
         }
-        arr[k].style.height = left[i];
+        arr[k].style.height = leftArr[i];
         i++;
         k++;
     }
@@ -65,30 +61,28 @@ async function merge(arr , l , r , m){
         else{
             arr[k].style.background = "rgba(238,174,202,1)";
         }
-        arr[k].style.height = right[j];
+        arr[k].style.height = rightArr[j];
         j++;
         k++;
     }
-
 }
 
+const mergeSort = async (arr,start,end)=>{
+    if(start >= end) return;
 
-async function mergeSort(arr , l , r){
-    if(l >= r){
-        return;
-    }
-    let mid = l + Math.floor((r-l)/2);
-    await mergeSort(arr , l , mid);
-    await mergeSort(arr , mid+1, r);
-    await merge(arr , l , r , mid);
+    let mid = start + Math.floor((end-start)/2);
+    await mergeSort(arr,start,mid);
+    await mergeSort(arr,mid+1,end);
+    await merge(arr,start,end,mid);
 }
-
 
 const mergeSortButton = document.querySelector("#mergeSort");
 mergeSortButton.addEventListener("click" , async function(){
     let arr = document.querySelectorAll(".barItem");
     let len = arr.length-1;
     ButtonsOff();
+    startTimer();
     await mergeSort(arr , 0 , len);
+    stopTimer();
     ButtonsOn();
 });
