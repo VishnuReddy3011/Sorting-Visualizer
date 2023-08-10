806471aef4bfad32319b24a5906646f51017d43a
Vishnu Vardhan Reddy Sajjala <sajjalavishnuvardhanreddy2001@gmail.com>
2023-08-10 17:25:53 +0530

    Update heapSort.js

--- JS/heapSort.js
+++ JS/heapSort.js
@@ -13,16 +13,15 @@ const heapify = async (arr,N,i)=>{
         arr[i].style.background = "lightseagreen";
         arr[largest].style.background = "lightseagreen";
         swap(arr[i],arr[largest]);
-        await wait(delay);
+        await wait(delay/2);
 
         arr[i].style.background = "lightsalmon";
         arr[largest].style.background = "lightsalmon";
-        await wait(delay);
+        await wait(delay/2);
 
         arr[i].style.background = "cornsilk";
         arr[largest].style.background = "cornsilk";
         await heapify(arr, N, largest);
-        await wait(delay);
     }
 }
 
@@ -34,10 +33,10 @@ const heapSort = async (arr,N)=>{
     }
     for(let i=N-1; i>=0; i--){
         arr[i].style.background = "-webkit-linear-gradient(rgba(238,174,202,1) 34%, rgba(148,187,233,1) 84%)";
-        await wait(delay/2);
+        await wait(delay/4);
         swap(arr[0], arr[i]);
         await heapify(arr,i,0);
-        await wait(delay);
+        await wait(delay/2);
     }
 }
 
