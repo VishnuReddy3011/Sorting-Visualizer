6152552ea1df2f43de3292d638244bc116a70279
Vishnu Vardhan Reddy Sajjala <sajjalavishnuvardhanreddy2001@gmail.com>
2023-08-11 08:34:40 +0530

    Update sort.js
    Updated for extra css effects.

--- JS/sort.js
+++ JS/sort.js
@@ -3,11 +3,24 @@ const swap = (a,b) => [a.style.height, b.style.height] = [b.style.height, a.styl
 const buttonIds = ["#newArray","#size_input","#bubbleSort","#mergeSort","#quickSort","#heapSort"];
 
 const ButtonsOn = () =>{
-    buttonIds.forEach((button)=>{document.querySelector(button).disabled = false;})
+    const boxes = document.querySelectorAll('.btns');
+
+    for (const box of boxes) {
+      box.classList.remove('bg-yellow');
+      box.classList.add('button');
+    }
+    buttonIds.forEach((btn)=>{document.querySelector(btn).disabled = false;})
 }
 
 const ButtonsOff = ()=>{
-    buttonIds.forEach((button)=>{document.querySelector(button).disabled = true;})
+    const boxes = document.querySelectorAll('.btns');
+
+    for (const box of boxes) {
+      box.classList.add('bg-yellow');
+      box.classList.remove('button');
+
+    }
+    buttonIds.forEach((btn)=>{document.querySelector(btn).disabled = true;})
 }
 
 let delay = 170;
